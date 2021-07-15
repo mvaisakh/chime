0e376dee7f2ae2a31dffe53cbcbbe4e7396e8c18
Phani Venkata Jayanth Kanakagiri <pkanakag@codeaurora.org>
2021-07-15 22:54:26 +0530

    Init: Added khaje post-boot script
    Added khaje post-boot script
    
    Change-Id: If9a52197beb0705939c564302e5a0c851dc89c20

--- base.mk
+++ base.mk
@@ -1218,7 +1218,8 @@ PRODUCT_PACKAGES_DEBUG += \
     init.qti.debug-trinket.sh \
     init.qti.debug-atoll.sh \
     init.qti.debug-lagoon.sh \
-    init.qti.debug-bengal.sh
+    init.qti.debug-bengal.sh \
+    init.qti.debug-khaje.sh
 
 PRODUCT_PACKAGES += liboemaids_system
 PRODUCT_PACKAGES += liboemaids_vendor
