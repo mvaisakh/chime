cbb68342ba627a272a667f94d5d336da5d1c5918
Vaisakh Murali <vaisakhmurali@gmail.com>
2022-10-13 13:22:23 +0600

    common: merge split makefiles
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- sm6115.mk
+++ sm6115.mk
@@ -1250,3 +1250,328 @@ SOONG_CONFIG_aosp_vs_qva_aosp_or_qva := qva
 else
 SOONG_CONFIG_aosp_vs_qva_aosp_or_qva := aosp
 endif
+
+$(call inherit-product, device/qcom/common/base.mk)
+
+# For PRODUCT_COPY_FILES, the first instance takes precedence.
+# Since we want use QC specific files, we should inherit
+# device-vendor.mk first to make sure QC specific files gets installed.
+$(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+ifeq ($(TARGET_BOARD_AUTO), true)
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+else
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+endif
+
+
+PRODUCT_BRAND := qcom
+PRODUCT_AAPT_CONFIG += hdpi mdpi
+
+PRODUCT_MANUFACTURER := QUALCOMM
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    persist.backup.ntpServer=0.pool.ntp.org \
+    sys.vendor.shutdown.waittime=500
+
+
+ifneq ($(TARGET_BOARD_AUTO), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.procedure_bytes=SKIP
+endif
+
+ifneq ($(ENABLE_HYP),true)
+ifneq ($(BOARD_FRP_PARTITION_NAME),)
+    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/$(BOARD_FRP_PARTITION_NAME)
+else
+    PRODUCT_PROPERTY_OVERRIDES += ro.frp.pst=/dev/block/bootdevice/by-name/config
+endif
+endif
+
+# whitelisted app
+PRODUCT_COPY_FILES += \
+    device/qcom/common/configs/permissions/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
+PRODUCT_COPY_FILES += \
+    device/qcom/common/configs/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
+ifneq ($(TARGET_BOARD_AUTO),true)
+#copy telephony app's permissions
+PRODUCT_COPY_FILES += $(QCPATH)/commonsys/telephony-build/build/telephony_product_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/telephony_product_privapp-permissions-qti.xml
+
+PRODUCT_COPY_FILES += $(QCPATH)/commonsys/telephony-build/build/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
+endif
+
+# Permission for Wi-Fi passpoint support
+PRODUCT_COPY_FILES += frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml
+
+PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
+
+ifneq ($(TARGET_DEFINES_DALVIK_HEAP), true)
+ifneq ($(TARGET_HAS_LOW_RAM), true)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+endif
+endif
+
+#$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
+#$(call inherit-product, frameworks/base/data/keyboards/keyboards.mk)
+
+# Enable AVB 2.0
+BOARD_AVB_ENABLE := true
+
+# Enable Virtual A/B
+ENABLE_VIRTUAL_AB := true
+
+ifeq ($(ENABLE_VIRTUAL_AB), true)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+endif
+
+# Default A/B configuration
+ENABLE_AB ?= true
+
+SYSTEMEXT_SEPARATE_PARTITION_ENABLE = true
+
+# Enable Dynamic partition
+BOARD_DYNAMIC_PARTITION_ENABLE ?= true
+
+SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := $(SHIPPING_API_LEVEL)
+
+BOARD_SHIPPING_API_LEVEL := 30
+BOARD_API_LEVEL := 30
+
+# For QSSI builds, we should skip building the system image. Instead we build the
+# "non-system" images (that we support).
+
+PRODUCT_BUILD_SYSTEM_IMAGE := false
+PRODUCT_BUILD_SYSTEM_OTHER_IMAGE := false
+PRODUCT_BUILD_VENDOR_IMAGE := true
+PRODUCT_BUILD_PRODUCT_IMAGE := false
+PRODUCT_BUILD_PRODUCT_SERVICES_IMAGE := false
+PRODUCT_BUILD_ODM_IMAGE := false
+ifeq ($(ENABLE_AB), true)
+PRODUCT_BUILD_CACHE_IMAGE := false
+else
+PRODUCT_BUILD_CACHE_IMAGE := true
+endif
+PRODUCT_BUILD_RAMDISK_IMAGE := true
+PRODUCT_BUILD_USERDATA_IMAGE := true
+
+TARGET_SKIP_OTA_PACKAGE := true
+
+#BUILD_BROKEN_PHONY_TARGETS := true
+BUILD_BROKEN_DUP_RULES := true
+TEMPORARY_DISABLE_PATH_RESTRICTIONS := true
+#export TEMPORARY_DISABLE_PATH_RESTRICTIONS
+
+ifneq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
+# Enable chain partition for system, to facilitate system-only OTA in Treble.
+BOARD_AVB_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_SYSTEM_ROLLBACK_INDEX := 0
+BOARD_AVB_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+else
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES += fastbootd
+# Add default implementation of fastboot HAL.
+PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
+# f2fs utilities
+PRODUCT_PACKAGES += \
+ sg_write_buffer \
+ f2fs_io \
+ check_f2fs
+
+# Userdata checkpoint
+PRODUCT_PACKAGES += \
+ checkpoint_gc
+
+ifeq ($(ENABLE_AB), true)
+# Userdata checkpoint start
+AB_OTA_POSTINSTALL_CONFIG += \
+RUN_POSTINSTALL_vendor=true \
+POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+FILESYSTEM_TYPE_vendor=ext4 \
+POSTINSTALL_OPTIONAL_vendor=true
+# Userdata checkpoint end
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
+else
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_non_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_non_AB_dynamic_partition.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
+endif
+BOARD_AVB_VBMETA_SYSTEM := system
+BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
+$(call inherit-product, build/make/target/product/gsi_keys.mk)
+endif
+
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_QCOM_FM := false
+TARGET_DISABLE_PERF_OPTIMIATIONS := false
+
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
+
+# Enable incremental FS feature
+PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
+
+PRODUCT_PACKAGES += init.qti.early_init.sh
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI
+
+# privapp-permissions whitelisting (To Fix CTS :privappPermissionsMustBeEnforced)
+PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
+
+TARGET_DEFINES_DALVIK_HEAP := true
+
+$(call inherit-product, device/qcom/vendor-common/common64.mk)
+# Temporary bring-up config <--
+
+# Temporary bring-up config -->
+PRODUCT_SUPPORTS_VERITY := false
+# Temporary bring-up config <--
+###########
+PRODUCT_PROPERTY_OVERRIDES  += \
+     dalvik.vm.heapstartsize=8m \
+     dalvik.vm.heapsize=256m \
+     dalvik.vm.heapgrowthlimit=128m \
+     dalvik.vm.heaptargetutilization=0.75 \
+     dalvik.vm.heapminfree=512k \
+     dalvik.vm.heapmaxfree=8m
+# Target naming
+PRODUCT_NAME := bengal
+PRODUCT_DEVICE := bengal
+PRODUCT_BRAND := qti
+PRODUCT_MODEL := Bengal for arm64
+
+
+TARGET_USES_AOSP := false
+TARGET_USES_AOSP_FOR_AUDIO := false
+TARGET_USES_QCOM_BSP := false
+
+# RRO configuration
+TARGET_USES_RRO := true
+
+TARGET_DISABLE_DISPLAY := false
+
+# Kernel configurations
+TARGET_KERNEL_VERSION := 4.19
+#Enable llvm support for kernel
+KERNEL_LLVM_SUPPORT := true
+#Enable sd-llvm support for kernel
+KERNEL_SD_LLVM_SUPPORT := true
+
+###########
+# Target configurations
+
+QCOM_BOARD_PLATFORMS += bengal
+
+TARGET_USES_QSSI := true
+
+#Default vendor image configuration
+ENABLE_VENDOR_IMAGE := true
+
+# default is nosdcard, S/W button enabled in resource
+PRODUCT_CHARACTERISTICS := nosdcard
+
+BOARD_FRP_PARTITION_NAME := frp
+
+# Android EGL implementation
+PRODUCT_PACKAGES += libGLES_android
+
+PRODUCT_PACKAGES += fs_config_files
+PRODUCT_PACKAGES += gpio-keys.kl
+PRODUCT_PACKAGES += libvolumelistener
+
+ifeq ($(ENABLE_AB), true)
+# A/B related packages
+PRODUCT_PACKAGES += update_engine \
+    update_engine_client \
+    update_verifier \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload
+# Boot control HAL test app
+PRODUCT_PACKAGES_DEBUG += bootctl
+
+PRODUCT_PACKAGES += \
+  update_engine_sideload
+
+endif
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/bengal/configs/vintf/framework_manifest.xml
+
+DEVICE_MANIFEST_FILE := device/qcom/bengal/configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE   := device/qcom/common/configs/vintf/compatibility_matrix.xml
+
+# Kernel modules install path
+KERNEL_MODULES_INSTALL := dlkm
+KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
+# MIDI feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
+
+#target specific runtime prop for qspm
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qspm.enable=true
+
+#FEATURE_OPENGLES_EXTENSION_PACK support string config file
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
+
+# Audio configuration file
+-include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/bengal/bengal.mk
+
+# MIDI feature
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
+
+#Enable full treble flag
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_VENDOR_MOVE_ENABLED := true
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+BOARD_VNDK_VERSION := current
+TARGET_MOUNT_POINTS_SYMLINKS := false
+
+PRODUCT_BOOT_JARS += telephony-ext
+PRODUCT_PACKAGES += telephony-ext
+
+PRODUCT_BOOT_JARS += tcmiface
+
+# Vendor property to enable advanced network scanning
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.enableadvancedscan=true
+
+# Property to disable ZSL mode
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=1
+
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.crypto.volume.filenames_mode = "aes-256-cts" \
+ro.crypto.allow_encrypt_override = true
+
+PRODUCT_PACKAGES += init.qti.dcvs.sh
+PRODUCT_PACKAGES += android.hardware.lights-service.qti
+
+#----------------------------------------------------------------------
+# wlan specific
+#----------------------------------------------------------------------
+include device/qcom/wlan/bengal/wlan.mk
+
+###################################################################################
+# This is the End of target.mk file.
+# Now, Pickup other split product.mk files:
+###################################################################################
+# TODO: Relocate the system product.mk files pickup into qssi lunch, once it is up.
+$(call inherit-product-if-exists, vendor/qcom/defs/product-defs/system/*.mk)
+$(call inherit-product-if-exists, vendor/qcom/defs/product-defs/vendor/*.mk)
+###################################################################################
