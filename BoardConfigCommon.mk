bc856635b6bbfb73e7ee23a80c0a5405f6bfe218
Vaisakh Murali <vaisakhmurali@gmail.com>
2022-10-16 21:05:04 +0600

    sm6115-common: rearrange cmdline for better readability
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,18 @@ TARGET_NO_KERNEL := false
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_BOOTIMG_HEADER_VERSION := 2
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x4a90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7
+BOARD_KERNEL_CMDLINE := \
+   console=ttyMSM0,115200n8 \
+   earlycon=msm_geni_serial,0x4a90000 \
+   androidboot.hardware=qcom \
+   androidboot.console=ttyMSM0 \
+   androidboot.memcg=1 \
+   lpm_levels.sleep_disabled=1 \
+   video=vfb:640x400,bpp=32,memsize=3072000 \
+   msm_rtb.filter=0x237 \
+   service_locator.enable=1 \
+   swiotlb=2048 \
+   loop.max_part=7
 TARGET_KERNEL_VERSION := 4.19
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
