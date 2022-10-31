5996afc807b9735d294400c1e78d03c300ee637c
Vaisakh Murali <vaisakhmurali@gmail.com>
2022-10-31 16:49:38 +0600

    sm6115-common: fix a small CAF derp
    This made all files specified to be copied before this point invalid!

--- sm6115.mk
+++ sm6115.mk
@@ -206,7 +206,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@2.0-service
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml\
